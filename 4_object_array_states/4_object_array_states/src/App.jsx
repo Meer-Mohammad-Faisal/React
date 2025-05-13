7a2a59b929c17814fe7a06a5d62f4634e13c9340
FAISAL <fmd35585@gmail.com>
2025-05-13 17:39:16 +0530

    objects and array

---
+++ 4_object_array_states/4_object_array_states/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LudoBoard from './LudoBoard';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LudoBoard />
+      </div>
+    </>
+  )
+}
+
+export default App
