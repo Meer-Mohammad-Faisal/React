47a6c1060d7a25d3868301de6b6d0045eed8ad21
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-10-23 22:53:15 +0530

    counter app

--- 03_useState_hook/03_counter/src/components/Counter.jsx
+++ 03_useState_hook/03_counter/src/components/Counter.jsx
@@ -6,7 +6,7 @@ const Counter = () => {
   return (
     <div className='counter-container'>
       <p id ="para">You have clicked {count} times</p>
-      <button id='btn' onClick= { () => {setCount(count + 1)}}>Click me</button>
+      <button id='btn' onClick={ () => {setCount(count + 1)}}>Click me</button>
     </div>
   )
 }
