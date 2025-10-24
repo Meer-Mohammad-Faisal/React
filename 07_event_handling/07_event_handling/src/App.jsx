35a29e74775285e29b0a16e12931d2255f972826
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-10-24 17:27:37 +0530

    Event Handling

--- 07_event_handling/07_event_handling/src/App.jsx
+++ 07_event_handling/07_event_handling/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   }
 
   function handleMouseOver() {
-    alert('i am mouse over');
+    alert('i am mouse over of para');
   }
 
   function handleInputChange(e) {
@@ -18,37 +18,26 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault(); // Prevent the default form submission behavior
+    // i am writing my custom behaviour
     alert('Form submitted');
   }
 
   return (
     <>
-
-
-    <button onClick={() => alert('i am clicked')}>
+    <button onClick={handleClick}>
       click me
     </button>
 
+    <p onMouseOver={handleMouseOver} style={{border:"1px solid black"}}>I am para</p>
+
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={ handleInputChange} />
+      <button type='submit'>Submit</button>
+    </form>
 {/* 
    <button onClick={alert('i am clicked')}>    this is imidiate invocation this will not work 
       click me
-    </button> */}
-
-
-
-
-{/* 
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={handleInputChange}/>
-      <button type='submit'>Submit</button>
-    </form> */}
-
-
-    {/* <p onMouseOver={handleMouseOver} style={{border:"1px solid black"}}>I am para</p>
-
-    <button onClick={handleClick}>
-      click me
-    </button> */}
+    </button> */} 
     </>
   )
 }
